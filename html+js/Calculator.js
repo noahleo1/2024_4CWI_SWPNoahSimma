808a073df7aceebfa6ec914a451cc499d2266737
noahleo1 <noah.simma@student.htldornbirn.at>
2023-10-10 09:27:47 +0200

    calculator

---
+++ html+js/Calculator.js
@@ -0,0 +1,9 @@
+function buttonClicked(){
+    console.log("clicked");
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    console.log(num1);
+    let result = parseFloat(num1) + parseFloat(num2);
+
+    console.log(result);
+}
