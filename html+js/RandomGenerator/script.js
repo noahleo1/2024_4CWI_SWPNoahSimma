4dea771269e7c2d634ce38f9ca6b266c98aefe68
noahleo1 <noah.simma@student.htldornbirn.at>
2023-11-16 11:01:12 +0100

    FußballAufgabe begonnen HTML Aufbau...

--- html+js/RandomGenerator/script.js
+++ html+js/RandomGenerator/script.js
@@ -5,6 +5,7 @@ function generate(){
     let amountOfRandomNum = parseInt(num2) - parseInt(num1);
     console.log(num1 + ":" + num2);
     console.log("clicked");
+    console.log("Info");
    let randomnum = Math.floor(Math.random() * amountOfRandomNum) + parseInt(num1);
    console.log("Die Generierte Zufallszahl zwischen den Werten:" + num1+" und "+num2+" ist: "+randomnum)
 }
