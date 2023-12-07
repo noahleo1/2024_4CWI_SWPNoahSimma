87d9a1eeca3a96bac9a9e41ffe4b1f3a40389442
noahleo1 <noah.simma@student.htldornbirn.at>
2023-12-07 17:25:21 +0100

    noch nicht ganz fertig probleme

--- AufgabeFigmaFußballTabelleNoahSimma/Fußball.js
+++ AufgabeFigmaFußballTabelleNoahSimma/Fußball.js
@@ -1,23 +1,27 @@
 function loadTable(){
-    fetch("https://api.openligadb.de/getbltable/PL/2010").
+    fetch("https://api.openligadb.de/getbltable/bl1/2023").
     then((res) =>res.json().then((result) => {
         insertTable(result);
     })
     );
 }
 
+
 function insertTable(data){
+    
     console.log("data",data);
+   
     const element = document.getElementById("maintable");
+   
 
     let output = '';
     data.forEach(team => {
          output += '<div id="table" class="flex p-4 gap-9">'
         
       
-        output +='<div>' + team.draw + '</div>'
+        output +='<div>' +"points:  "+team.points+'</div>'
         output +='<div>' + team.teamName+ '</div>'
-        output +='<div>' + team.points+ '</div>'
+        
         output +='<div><img width="20px" src="' + team.teamIconUrl + '"/></div>'
         output += '</div>';
     });
