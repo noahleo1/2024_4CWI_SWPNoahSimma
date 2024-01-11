b13e4f458b50bef2be0ada0aee53db7182d10c19
noahleo1 <noah.simma@student.htldornbirn.at>
2024-01-11 13:28:15 +0100

    React Basics mit Youtube Video Gemacht

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="bg-green-400" >
     <h1>Hello</h1>
-     
+     <Button name="Hans" age="17" /> 
+     <Button name="Sepp"/>
+     <Button name="Paul"/>
     </div>
   );
 }
---
+++ reactapp/src/components/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function button(props) {
+  return (
+    <div>{props.name} is {props.age} years old </div>
+  )
+}
