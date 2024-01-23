feffafe88232b6b38e766c07d71826da86e2a029
noahleo1 <noah.simma@student.htldornbirn.at>
2024-01-23 09:24:55 +0100

    cardsaufgabefertig

--- noahsreact-app/src/components/PeopleContainer.js
+++ noahsreact-app/src/components/PeopleContainer.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './card'
 
 export default function PeopleContainer() {
+  const[ people, setPeople] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://65af65cf2f26c3f2139a9e9c.mockapi.io/person").then(
+      (res) => res.json().then((data) => {
+        setPeople(data);
+      })
+    );
+  }, [])
   return (
     <div>
         <h1>people  </h1>
-        <div>
-        <Card name="hans" title="ceo" imageUrl="https://picsum.photos/200/300"/>
-        <Card name="hans" title="ceo" imageUrl="https://picsum.photos/200/300"/>
-        <Card name="hans" title="ceo" imageUrl="https://picsum.photos/200/300"/>
-        </div>
+        <div className='grid grid-cols-2 lg:grid-cols-4  gap-4'>
+         
+        {people.map(element => {
+          return <Card name={personalbar.name} imageUrl={personalbar.avatar} title={personalbar.jobtitle}/>
+        })}</div>
     </div>
   )
 }
--- noahsreact-app/src/components/card.js
+++ noahsreact-app/src/components/card.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function card(name, title, imageUrl ) {
   return (
     <div className='border grid grid-cols-2 h-40'>
-        <div className='p-4'>
+        <div className='flex-1 p-4'>
             <h2 className='font-bold text-xl'>{Name}</h2>
             <h4>{title}</h4>
         </div>
-        <div className='bg-green-400 w-full'>
+        <div className='bg-green-400 w-full flex-1'>
 
             <img src={imageUrl}alt='' className='object-cover w-full h-full' />
         </div>
